fcb7b6af23832e17819865b2f4a9c2bcf92e514e
nelequadvlieg <nele.quadvlieg@hotmail.com>
2025-11-07 14:09:02 +0100

    change 2.1

--- signals.py
+++ signals.py
@@ -1,11 +1,35 @@
 import numpy as np 
 
 def generate_sine_wave(frequency, duration, time_shift, time_scale):
+    """
+    Generate a sinusoidal wave signal.
+    
+    Parameters:
+    frequency (float): Frequency of the sine wave in Hz
+    duration (float): Duration of the signal in seconds
+    time_shift (float): Time shift applied to the signal
+    time_scale (float): Time scaling factor
+    
+    Returns:
+    tuple: (time_array, wave_array) containing time values and corresponding wave amplitudes
+    """
     t = np.linspace(0, duration, 100)
     wave = np.sin(2 * np.pi * frequency * t * time_scale + time_shift)
     return t, wave 
 
 def generate_step_function(t, step_time):
+    """
+    Generate a sinusoidal wave signal.
+    
+    Parameters:
+    frequency (float): Frequency of the sine wave in Hz
+    duration (float): Duration of the signal in seconds
+    time_shift (float): Time shift applied to the signal
+    time_scale (float): Time scaling factor
+    
+    Returns:
+    tuple: (time_array, wave_array) containing time values and corresponding wave amplitudes
+    """
     return np.where(t < step_time, 0, 1)
 
 exit()
